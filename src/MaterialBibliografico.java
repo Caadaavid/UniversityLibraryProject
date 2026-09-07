39eeecf9e12b1ff7a503d95dfcd22c5f0a3dd9d6
Cadavid !! <juandanielcadavid0702@gmail.com>
2026-09-06 22:36:11 -0500

    Reto 2 completado

--- src/MaterialBibliografico.java
+++ src/MaterialBibliografico.java
@@ -5,10 +5,22 @@ public class MaterialBibliografico {
 
     private static int contadorMateriales = 0;
 
+    //Este contador aumenta cada que se crea un objeto, pero no depende de los objetos como tal, sino de la clase.
+    public static int getContadorMateriales() {
+        return contadorMateriales;
+    }
+
     public MaterialBibliografico(String codigo, String titulo, int publicado){
         this.codigo = codigo;
         this.titulo = titulo;
         this.publicado = publicado;
+        contadorMateriales++;
+    }
+
+    public MaterialBibliografico(String codigo, String titulo){
+        this.codigo = codigo;
+        this.titulo = titulo;
+        this.publicado = 0;
         contadorMateriales++;
     }
 
